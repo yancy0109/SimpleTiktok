f752265b25df53db8c047e8ed08b76c9eca35bb9
yancy0109 <1156729237@qq.com>
2022-06-01 00:21:07 +0800

    创建通用response

--- controller/publish.go
+++ controller/publish.go
@@ -2,17 +2,13 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/yancy0109/SimpleTiktok/common"
 	"net/http"
 )
 
-type respones struct {
-	StatusCode int32  `json:"status_code"`
-	StatusMsg  string `json:"status_msg"`
-}
-
 func Publish(context *gin.Context) {
 	context.JSON(http.StatusOK,
-		&respones{
+		&common.Response{
 			StatusCode: 1,
 			StatusMsg:  "拿捏了",
 		})

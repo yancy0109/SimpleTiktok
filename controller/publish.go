5dc12f86cd573a52199f88186e097cc65275dfed
yancy0109 <1156729237@qq.com>
2022-06-01 08:52:58 +0800

    视频上传

--- controller/publish.go
+++ controller/publish.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/yancy0109/SimpleTiktok/service"
 	"net/http"
+	"path/filepath"
 )
 
 type Response struct {
@@ -11,9 +13,45 @@ type Response struct {
 }
 
 func Publish(context *gin.Context) {
-	context.JSON(http.StatusOK,
-		&Response{
+	//解析context中 data token title
+	title := context.PostForm("title")
+	token := context.PostForm("token")
+	//检验token获取user
+	var userid int
+	if token != "" {
+		userid = 2123123
+	}
+	data, err := context.FormFile("data")
+	//接收文件失败返回
+	if err != nil {
+		context.JSON(http.StatusOK, Response{
 			StatusCode: 1,
-			StatusMsg:  "拿捏了",
+			StatusMsg:  err.Error(),
 		})
+	}
+	//对文件进行处理
+	//1.获取文件名字
+	filename := data.Filename
+	//2.生成文件保存名
+	finalname := "user11_" + filename
+	//3.生成保存路径
+	saveFile := filepath.Join("./public/", finalname)
+	//开始保存
+	if err := context.SaveUploadedFile(data, finalname); err != nil {
+		context.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  err.Error(),
+		})
+	}
+	//调取service层存储数据至数据库
+	if err := service.SavePublish(userid, saveFile, "", title); err != nil {
+		context.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  "保存失败",
+		})
+	}
+	context.JSON(http.StatusOK, Response{
+		StatusCode: 0,
+		StatusMsg:  filename + "uploaded successfully",
+	})
 }
---
+++ service/publish.go
@@ -0,0 +1,6 @@
+package service
+
+func SavePublish(author_id int, play_url string, cover_url string, title string) error {
+
+	return nil
+}

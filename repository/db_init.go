d6f8f6ddeb3d7953c534d4d854b2335c48bb6674
iwnfy <2317937320@qq.com>
2022-06-03 20:05:08 +0800

    新增环境配置文件

--- repository/db_init.go
+++ repository/db_init.go
@@ -1,22 +1,35 @@
 package repository
 
 import (
+	"fmt"
+	"github.com/subosito/gotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"os"
 )
 
 var db *gorm.DB
 
 func Init() error {
-	var err error
+	err := gotenv.Load()
+	if err != nil {
+		return err
+	}
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASSWORD")
+	dbname := os.Getenv("MYSQL_DBNAME")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	dsn := user + ":" + pass + "@tcp(" + host + ":" + port + ")/" + dbname + "?charset=utf8&parseTime=True&loc=Local"
+	fmt.Println(dsn)
 	db, err = gorm.Open(mysql.New(mysql.Config{
-		DSN:                       "root:556832lk@tcp(localhost:3306)/douyin?charset=utf8&parseTime=True&loc=Local", // DSN data source name
-		DefaultStringSize:         256,                                                                              // string 类型字段的默认长度
-		DisableDatetimePrecision:  true,                                                                             // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
-		DontSupportRenameIndex:    true,                                                                             // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
-		DontSupportRenameColumn:   true,                                                                             // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
-		SkipInitializeWithVersion: false,                                                                            // 根据当前 MySQL 版本自动配置
+		DSN:                       dsn,   // DSN data source name
+		DefaultStringSize:         256,   // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
 	}), &gorm.Config{
 		//开启数据库语句log
 		Logger: logger.Default.LogMode(logger.Info),

fc5cae718295e7ba8375c22d4cfebf732371dc14
iwnfy <2317937320@qq.com>
2022-06-03 17:27:42 +0800

    添加 favoriteDao.favorite 操作 返回 当前点赞状态

--- repository/favorite.go
+++ repository/favorite.go
@@ -29,11 +29,22 @@ func NewfavoriteDaoInstance() *FavoriteDao {
 func (FavoriteRecord) TableName() string {
 	return "video_favorite"
 }
+
 func (*FavoriteDao) GetList(favoriteRecord FavoriteRecord) []FavoriteRecord {
 	var results []FavoriteRecord
 	db.Table("video_favorite").Where("video_id = ? AND Status = ?", favoriteRecord.Userid, 1).Find(&results) // (*sql.Row)
 	return results
 }
+
+//返回是否点赞
+func (*FavoriteDao) favorite(favoriteRecord FavoriteRecord) bool {
+	var id int64
+	//查询id是否存在
+	db.Raw("SELECT id FROM video_favorite WHERE user_id = ? AND video_id = ? AND Status = 1 ", favoriteRecord.Userid, favoriteRecord.Videoid).Scan(&id)
+	// == 0 表示不存在该记录
+	return id != 0
+}
+
 func (*FavoriteDao) GetCount(favoriteRecord FavoriteRecord) int64 {
 	var count int64
 	db.Model(&favoriteRecord).Where("video_id = ? AND Status = ? ", favoriteRecord.Videoid, 1).Count(&count)
@@ -43,17 +54,17 @@ func (*FavoriteDao) GetCount(favoriteRecord FavoriteRecord) int64 {
 
 func (*FavoriteDao) UpdateStatus(favoriteRecord FavoriteRecord) error {
 	var id int64
-	//查询id是否存在
+	//查询记录是否存在
 	db.Raw("SELECT id FROM video_favorite WHERE user_id = ? AND video_id = ?", favoriteRecord.Userid, favoriteRecord.Videoid).Scan(&id)
 	if id == 0 {
 		favoriteRecord.Createdate = time.Now()
-		err := db.Model(&favoriteRecord).Create(&favoriteRecord)
+		err := db.Model(&favoriteRecord).Create(&favoriteRecord) //记录不存在-创建
 		if err != nil {
 			return err.Error
 		}
 	}
 	favoriteRecord.Id = id
-	err := favoriteDao.ChangeStatus(favoriteRecord)
+	err := favoriteDao.ChangeStatus(favoriteRecord) //记录存在-更新
 	if err != nil {
 		return err
 	}

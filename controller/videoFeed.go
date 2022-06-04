febfc19af34ff2df33a2a836696ed68b8537b707
XiaoKang <1320342065@qq.com>
2022-06-04 09:59:13 +0800

    the controller for videoFeed

--- controller/videoFeed.go
+++ controller/videoFeed.go
@@ -1,47 +1,26 @@
 package controller
 
 import (
-	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/yancy0109/SimpleTiktok/middleware"
+	"github.com/yancy0109/SimpleTiktok/service"
+	"net/http"
+	"strconv"
 )
 
-type VideoFeed struct {
-	NextTime   *int64  `json:"next_time"`   // 本次返回的视频中，发布最早的时间，作为下次请求时的latest_time
-	StatusCode int64   `json:"status_code"` // 状态码，0-成功，其他值-失败
-	StatusMsg  *string `json:"status_msg"`  // 返回状态描述
-	VideoList  []Video `json:"video_list"`  // 视频列表
-}
-
-// Video
-type Video struct {
-	Author        User   `json:"author"`         // 视频作者信息
-	CommentCount  int64  `json:"comment_count"`  // 视频的评论总数
-	CoverURL      string `json:"cover_url"`      // 视频封面地址
-	FavoriteCount int64  `json:"favorite_count"` // 视频的点赞总数
-	ID            int64  `json:"id"`             // 视频唯一标识
-	IsFavorite    bool   `json:"is_favorite"`    // true-已点赞，false-未点赞
-	PlayURL       string `json:"play_url"`       // 视频播放地址
-	Title         string `json:"title"`          // 视频标题
-}
-
-// 视频作者信息
-//
-// User
-type User struct {
-	FollowCount   int64  `json:"follow_count"`   // 关注总数
-	FollowerCount int64  `json:"follower_count"` // 粉丝总数
-	ID            int64  `json:"id"`             // 用户id
-	IsFollow      bool   `json:"is_follow"`      // true-已关注，false-未关注
-	Name          string `json:"name"`           // 用户名称
-}
-type VideoService struct {
-}
-
-func UnmarshalVideoFeed(data []byte) (VideoFeed, error) {
-	var r VideoFeed
-	err := json.Unmarshal(data, &r)
-	return r, err
-}
-
-func (r *VideoFeed) Marshal() ([]byte, error) {
-	return json.Marshal(r)
+func VideoFeed(context *gin.Context) {
+	token := context.Query("token")
+	//检查token
+	userId, err := middleware.ParseToken(token)
+	if err != nil {
+		context.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  "token无效",
+		})
+		return
+	}
+	latestTime, _ := strconv.Atoi(context.Query("latest_time"))
+	var videoService service.VideoService
+	videoFeed := videoService.GetVideoFeed(latestTime, userId)
+	context.JSON(http.StatusOK, videoFeed)
 }
--- service/videoFeed.go
+++ service/videoFeed.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 )
 
-type videoFeed struct {
+type VideoFeed struct {
 	NextTime   *int64  `json:"next_time"`   // 本次返回的视频中，发布最早的时间，作为下次请求时的latest_time
 	StatusCode int64   `json:"status_code"` // 状态码，0-成功，其他值-失败
 	StatusMsg  *string `json:"status_msg"`  // 返回状态描述
@@ -39,8 +39,8 @@ type VideoService struct {
 
 var videoListDao = repository.NewVideoListInstance()
 
-func (*VideoService) GetVideoFeed(latestTime int, userId int64) *videoFeed {
-	var videoFeed videoFeed
+func (*VideoService) GetVideoFeed(latestTime int, userId int64) *VideoFeed {
+	var videoFeed VideoFeed
 	videos, _ := videoListDao.VideoList(latestTime)
 	videoList := make([]Video, len(videos))
 	var waitGroup sync.WaitGroup
